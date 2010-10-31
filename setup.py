8ed70832fee135e8eaf369a8c9e7c69a1b3072c0
Alexander Dutton <dev@alexdutton.co.uk>
2010-10-31 15:06:55 +0000

    Added a setup.py.

---
+++ setup.py
@@ -0,0 +1,20 @@
+from distutils.core import setup
+
+setup(
+    name = 'talisprism',
+    version = '0.1',
+    url = 'http://github.com/alexsdutton/talis-prism-api',
+    author = 'Alexander Dutton',
+    author_email = 'dev@alexdutton.co.uk',
+    description ="A simple wrapper around a Talis Prism patron services HTTP interface.",
+    packages = ['talisprism'],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'License :: Public Domain',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python',
+        'Topic :: Education',
+        'Topic :: Internet :: WWW.HTML',
+    ],
+)
+
